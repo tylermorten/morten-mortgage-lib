15f24b6b688b1315268d891c74b4fcb4dc1261f3
Tyler Morten <tyler@agileninjas.com>
2015-01-02 14:36:52 -0600

    Initial code

---
+++ src/morten_mortgage_lib/core.clj
@@ -0,0 +1,33 @@
+(ns morten-mortgage-lib.core)
+
+(def WAC 3)
+
+(defn G [] (fn [] (/ WAC 1200)))
+(defn U [] (/ 1 (+ 1 ((G)))))
+(defn PAY [N] (/ ((G)) (- 1 (Math/pow (U) N))))
+
+(declare R)
+
+(defn BAL [N n]
+  (let [R (- N n)]
+    (/ (- 1 (Math/pow (U) R))
+       (- 1 (Math/pow (U) N)))))
+
+(defn R [N n] (- N n))
+
+(defn PRIN [N n]
+  (let [r (R N n)]
+    (/ (* ((G)) (Math/pow (U) (+ 1 r)))
+       (- 1 (Math/pow (U) N)))))
+
+(defn INT [N n]
+  (let [r (R N n)]
+    (/ (* ((G)) (- 1 (Math/pow (U) (+ 1 r))))
+       (- 1 (Math/pow (U) N)))))
+
+(def PRINC 149.826544)
+
+(* PRINC (PRIN 360 360))
+
+(defn B [N n] (* PRINC (BAL N n)))
+(defn Q [N n F] (/ F (B N n)))
